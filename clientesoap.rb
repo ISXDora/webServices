8d6f81ee1be60035f84419c56ffe2611c7af8e00
ISXDora <isinhasena@gmail.com>
2021-08-26 10:22:24 -0300

    Cliente consumindo webservice com Ruby usando Savon

--- clientesoap.rb
+++ clientesoap.rb
@@ -3,6 +3,9 @@ client = Savon.client(wsdl: "https://www.dataaccess.com/webservicesserver/Number
 puts "Mostrando as operações desse webservice: "
 puts client.operations
 puts "\n"
-response = client.call(:number_to_words, message: {"ubiNum" => "14"})
-puts response.body[ :number_to_words_response][ :numbers_to_words_result => ""]
+puts "Digite um número em formato decimal, separado por ponto:"
+input = gets.chomp
+response = client.call(:number_to_dollars, message: {"dNum" => input })
+puts "A resposta é: " +  response.body[ :number_to_dollars_response][ :number_to_dollars_result]
+
 puts "\n"
