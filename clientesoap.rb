498746f969be52081736fc19d9b2affefcd13990
ISXDora <isinhasena@gmail.com>
2021-08-25 21:37:04 -0300

    third commit - obtendo resposta via protoc soap

--- clientesoap.rb
+++ clientesoap.rb
@@ -3,3 +3,6 @@ client = Savon.client(wsdl: "https://www.dataaccess.com/webservicesserver/Number
 puts "Mostrando as operações desse webservice: "
 puts client.operations
 puts "\n"
+response = client.call(:number_to_words, message: {"ubiNum" => "14"})
+puts response.body[ :number_to_words_response][ :numbers_to_words_result => ""]
+puts "\n"
